747e223173144b1a138e6dfd65fe3ebfa805c2b4
co1010 <colinclarke1010@gmail.com>
2020-10-09 16:47:29 -0400

    Added some initialization to main

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"MP3/nodes"
+	"MP3/utils"
+)
 
+func main() {
+	// Initialize config struct
+	config := utils.CreateConfigStruct()
+
+	// Initialize node processes
+	nodes.InitializeNodes(config.Nodes)
+
+	// Initialize connections between nodes
+	nodes.InitializeConnections()
 }
